5d85bb3bdac8816de0e19841ca33bdbfa228b557
christianByabushi <murhchristian@gmail.com>
2024-07-24 05:56:09 +0200

    problems page

--- application/models.py
+++ application/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -8,6 +9,7 @@ class AccountType(models.TextChoices):
     COLLECTOR = 'COLLECTOR', 'Collector'
     USER = 'USER', 'User'
 
+
 class Gender(models.TextChoices):
     MALE = 'M', 'Male'
     FEMALE = 'F', 'Female'
@@ -15,14 +17,114 @@ class Gender(models.TextChoices):
 
 
 class UserProfile(AbstractUser):
-    avatar = models.ImageField(upload_to='images/users/', default='assets/img/avatar.png')
+    avatar = models.ImageField(
+        upload_to='images/users/', default='assets/img/avatar.png')
     description = models.TextField(blank=True, null=True)
     title = models.CharField(max_length=255, null=True)
-    gender = models.CharField(max_length=1, choices=Gender.choices, default=Gender.OTHER)
-    account_type = models.CharField(max_length=10, choices=AccountType.choices, default=AccountType.USER)
-    province = models.CharField(max_length=50,null=False)
+    gender = models.CharField(
+        max_length=1, choices=Gender.choices, default=Gender.OTHER)
+    account_type = models.CharField(
+        max_length=10, choices=AccountType.choices, default=AccountType.USER)
+    province = models.CharField(max_length=50, null=False)
     city = models.CharField(max_length=50, null=False)
-    role = models.CharField(max_length=50,null=False)
+    role = models.CharField(max_length=50, null=False)
 
     def __str__(self):
         return self.username
+
+    def get_avatar_url(self):
+        if self.avatar:
+            return self.avatar.url
+        return None
+
+
+class Femme(models.Model):
+    MENAGE_CHOICES = [
+        ('Bushwira', 'Bushwira'),
+        ('Mbobero', 'Mbobero'),
+        ('Kagabi', 'Kagabi')
+    ]
+
+    GENERE_PROBLEMS = [
+        ('Financier', 'Financier'),
+        ('Social', 'Social'),
+        ('Culturel', 'Culturel'),
+        ('Grossesse', 'Grossesse'),
+    ]
+
+    APPLIQUER_CHOICES = [
+        ('1ère fois', '1ère fois'),
+        ('2e fois', '2e fois'),
+        ('3e fois', '3e fois')
+    ]
+
+    SCOLARITE_CHOICES = [
+        ('Jamais scolarisée', 'Jamais scolarisée'),
+        ('Primaire', 'Primaire'),
+        ('Secondaire', 'Secondaire'),
+        ('Universitaire/Supérieur', 'Universitaire/Supérieur')
+    ]
+    menage = models.CharField(max_length=20, choices=MENAGE_CHOICES)
+    appliquer_questionnaire = models.CharField(
+        max_length=20, choices=APPLIQUER_CHOICES)
+    name_femme = models.CharField(max_length=100)
+    numero_tel_femme = models.CharField(max_length=20)
+    form_file = models.ImageField(
+        upload_to='femme_images/', blank=True, null=True)
+    age_femme = models.IntegerField()
+    scolarite_femme = models.CharField(
+        max_length=50, choices=SCOLARITE_CHOICES)
+    genreProbleme = models.CharField(
+        max_length=50, null=True, choices=GENERE_PROBLEMS)
+    descriptionCasFemme = models.TextField()
+    titleCase = models.CharField(max_length=100, blank=False, null=False)
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name_femme
+
+    def get_image_url(self):
+        if self.form_file:
+            return self.form_file.url
+        return None
+
+
+class Problem(models.Model):
+    GENRE_CHOICES = [
+        ('Financier', 'Financier'),
+        ('Social', 'Social'),
+        ('Culturel', 'Culturel'),
+    ]
+
+    PROVINCE_CHOICES = [
+        ('Bukavu', 'Bukavu'),
+        ('Kabare', 'Kabare'),
+        ('Walungu', 'Walungu'),
+        ('Fizi', 'Fizi'),
+        ('Mwenga', 'Mwenga'),
+    ]
+
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    genre_probleme = models.CharField(max_length=20, choices=GENRE_CHOICES)
+    date_probleme = models.DateField()
+    time_probleme = models.TimeField()
+    description_cas_probleme = models.TextField()
+    province_cas_probleme = models.CharField(
+        max_length=20, choices=PROVINCE_CHOICES)
+    precision_endroit_probleme = models.CharField(max_length=255)
+    anticipation_aide = models.TextField()
+    acteur_cause_probleme = models.CharField(max_length=255)
+    appliquer_questionnaire = models.TextField()
+    presumes_acteurs = models.TextField()
+    problem_victiomes = models.TextField()
+    consequences_problemes = models.TextField()
+    images_preuves_probleme = models.ImageField(
+        upload_to='problem_images/', blank=True, null=True)
+
+    def __str__(self):
+        return self.genre_probleme
+
+    def get_image_url(self):
+        if self.images_preuves_probleme:
+            return self.images_preuves_probleme.url
+        return None
--- application/views.py
+++ application/views.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.models import User, Group
+from django.core.paginator import Paginator
+from .models import Femme
 import json
 from django.contrib.auth import get_user_model
 from django.views.decorators.csrf import csrf_exempt
@@ -23,6 +26,110 @@ def handleWomen(request):
 
 
 @login_required
+def actions(request):
+    return render(request, 'collectdata/index.html')
+
+
+@csrf_exempt
+def create_femme(request):
+    if request.method == 'POST':
+        menage = request.POST.get('menage')
+        appliquer_questionnaire = request.POST.get('appliquerQuestionnaire')
+        name_femme = request.POST.get('nameFemme')
+        numero_tel_femme = request.POST.get('numeroTelFemme')
+        form_file = request.FILES.get('formFile')
+        age_femme = request.POST.get('ageFemme')
+        scolarite_femme = request.POST.get('scolariteFemme')
+
+        if not (menage and appliquer_questionnaire and name_femme and numero_tel_femme and age_femme and scolarite_femme):
+            return JsonResponse({'message': 'Erreur : Tous les champs sont requis.'}, status=400)
+
+        try:
+            femme = Femme.objects.create(
+                menage=menage,
+                appliquer_questionnaire=appliquer_questionnaire,
+                name_femme=name_femme,
+                numero_tel_femme=numero_tel_femme,
+                form_file=form_file,
+                age_femme=age_femme,
+                scolarite_femme=scolarite_femme
+            )
+            return JsonResponse({'message': 'Femme created successfully.'}, status=201)
+        except Exception as e:
+            return JsonResponse({'message': str(e)}, status=400)
+    else:
+        return JsonResponse({'message': 'Invalid request method.'}, status=405)
+
+
+@login_required
+def femmes(request):
+    if request.method == 'POST':
+        # Extracting form data
+        menage = request.POST.get('menage')
+        appliquer_questionnaire = request.POST.get('appliquerQuestionnaire')
+        name_femme = request.POST.get('nameFemme')
+        numero_tel_femme = request.POST.get('numeroTelFemme')
+        form_file = request.FILES.get('formFile')
+        age_femme = request.POST.get('ageFemme')
+        scolarite_femme = request.POST.get('scolariteFemme')
+        genre_probleme = request.POST.get('genreProbleme')
+        description_cas_femme = request.POST.get('descriptionCasFemme')
+        titleCase = request.POST.get('titleCase')
+
+        # Check if required fields are present
+        if not (menage and appliquer_questionnaire and name_femme and numero_tel_femme and age_femme and scolarite_femme and genre_probleme and description_cas_femme):
+            return JsonResponse({'message': 'Erreur : Tous les champs sont requis.'}, status=400)
+
+        # Check if the uploaded file is an image
+        if form_file:
+            if not form_file.content_type.startswith('image/'):
+                return JsonResponse({'message': 'Erreur : Le fichier doit \être une image.'}, status=400)
+
+        # Save to the database
+        Femme.objects.create(
+            menage=menage,
+            appliquer_questionnaire=appliquer_questionnaire,
+            name_femme=name_femme,
+            numero_tel_femme=numero_tel_femme,
+            form_file=form_file,
+            age_femme=age_femme,
+            scolarite_femme=scolarite_femme,
+            genreProbleme=genre_probleme,
+            titleCase=titleCase,
+            descriptionCasFemme=description_cas_femme,
+            user=request.user
+        )
+
+        return JsonResponse({'message': f'Les infos sur la femme : {name_femme} ont été enregistrées avec succes.'}, status=201)
+
+    else:
+        selected_user_id = request.GET.get('userCollector')
+        selected_user_object = None
+        if request.user.groups.filter(name='collecter').exists():
+            entries = Femme.objects.filter(user=request.user)
+            # if the user is not a collector
+        else:
+            entries = Femme.objects.all()
+            if selected_user_id:
+                entries = entries.filter(user_id=selected_user_id)
+                selected_user_object = UserProfile.objects.get(
+                    pk=selected_user_id)
+
+        is_supervisor = request.user.groups.filter(name='supervisor').exists()
+
+        paginator = Paginator(entries, 5)
+        page_number = request.GET.get('page')
+        femmes_entries = paginator.get_page(page_number)
+        context = {
+            'femmesEntries': femmes_entries,
+            'Allusers': UserProfile.objects.all(),
+            'is_supervisor': is_supervisor,
+            'selected_user_object': selected_user_object
+        }
+        return render(request, 'collectdata/femmes.html', context)
+
+
+@login_required
 def collectiondata(request):
     return render(request, 'welcome/tableau-de-bord.html')
 
@@ -35,6 +142,52 @@ def boiteSuggestion(request):
 @login_required
 def agentsAmis(request):
     return render(request, 'welcome/tableau-de-bord.html')
+
+
+def problems(request):
+    if request.method == 'POST':
+        genre_probleme = request.POST.get('genreProbleme')
+        date_probleme = request.POST.get('dateProblem')
+        time_probleme = request.POST.get('dateProblemTime')
+        description_cas_probleme = request.POST.get('descriptionCasProblem')
+        province_cas_probleme = request.POST.get('provinceCasProblem')
+        precision_endroit_probleme = request.POST.get(
+            'precisionEndroitProblem')
+        anticipation_aide = request.POST.get('anticipationAide')
+        acteur_cause_probleme = request.POST.get('acteurCauseProbleme')
+        appliquer_questionnaire = request.POST.get('appliquerQuestionnaire')
+        presumes_acteurs = request.POST.get('presumesActeurs')
+        problem_victiomes = request.POST.get('problemVictiomes')
+        consequences_problemes = request.POST.get('consequencesProblemes')
+        images_preuves_probleme = request.FILES.get('imagesPreuvesProblem')
+
+        problemData = Problem(
+            user=request.user,
+            genre_probleme=genre_probleme,
+            date_probleme=date_probleme,
+            time_probleme=time_probleme,
+            description_cas_probleme=description_cas_probleme,
+            province_cas_probleme=province_cas_probleme,
+            precision_endroit_probleme=precision_endroit_probleme,
+            anticipation_aide=anticipation_aide,
+            acteur_cause_probleme=acteur_cause_probleme,
+            appliquer_questionnaire=appliquer_questionnaire,
+            presumes_acteurs=presumes_acteurs,
+            problem_victiomes=problem_victiomes,
+            consequences_problemes=consequences_problemes,
+            images_preuves_probleme=images_preuves_probleme,
+        )
+        print(request.POST)
+        #problemData.save()
+
+        return JsonResponse({'message': 'Informations sur le problème créé avec succès.'}, status=201)
+    else:
+        is_supervisor = request.user.groups.filter(name='supervisor').exists()
+        context = {
+            'is_supervisor': is_supervisor,
+            'Allusers': UserProfile.objects.all()
+        }
+        return render(request, 'collectdata/problems.html', context)
 
 
 User = get_user_model()
